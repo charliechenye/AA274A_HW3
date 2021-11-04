3577910a7956cd9628c8dbb798323232c5f99fb0
Chenye Zhu <zhuchenye@gmail.com>
2021-11-04 03:14:35 -0700

    Extra Problem

--- Extra_Problem/template_matching.py
+++ Extra_Problem/template_matching.py
@@ -4,6 +4,8 @@ import numpy as np
 import time
 import cv2
 import matplotlib.pyplot as plt
+from os.path import join
+
 
 
 def template_match(template, image, threshold=0.999):
@@ -17,7 +19,11 @@ def template_match(template, image, threshold=0.999):
         matches: A list of (top-left y, top-left x, bounding box height, bounding box width) tuples for each match's bounding box.
     """
     ########## Code starts here ##########
-    raise NotImplementedError("Implement me!")
+    h, w, _ = template.shape
+    results = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+    threshold_met_boxes = np.argwhere((results >= threshold))
+    matches = [(box_i[0], box_i[1], h, w) for box_i in threshold_met_boxes]
+    return matches
     ########## Code ends here ##########
 
 
@@ -38,8 +44,10 @@ def create_and_save_detection_image(image, matches, filename="image_detections.p
 
 
 def main():
+    # for Waldo
     image = cv2.imread('clutter.png').astype(np.float32)
     template = cv2.imread('valdo.png').astype(np.float32)
+
 
     matches = template_match(template, image)
     create_and_save_detection_image(image, matches)
@@ -47,7 +55,7 @@ def main():
     template = cv2.imread('stop_signs/stop_template.jpg').astype(np.float32)
     for i in range(1, 6):
         image = cv2.imread('stop_signs/stop%d.jpg' % i).astype(np.float32)
-        matches = template_match(template, image, threshold=0.85)
+        matches = template_match(template, image, threshold=0.6)
         create_and_save_detection_image(image, matches, 'stop_signs/stop%d_detection.png' % i)
 
 
